92282db0e172e32dc41fd57c8f4b8b7399351b69
albertomtferreira <60177437+albertomtferreira@users.noreply.github.com>
2020-04-04 17:01:39 +0100

    update

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ export const Card  = (props) => (
     <div className='card-container'>
         <img className='flag-container' alt={`${props.country.countryInfo.iso3}`} src={props.country.countryInfo.flag}/>
         <h1 >Country: {props.country.country}</h1>
-        <h2 >Total Cases:{props.country.cases}</h2>
+        <h2 >Total Cases: {props.country.cases}</h2>
         <h2 >Total Deaths: {props.country.deaths}</h2>
         <h3 >Total Active: {props.country.active}</h3>
     </div>
