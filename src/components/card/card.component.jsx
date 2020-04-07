77b7a8775e923c24266f79916e6ae567a8b93330
albertomtferreira <60177437+albertomtferreira@users.noreply.github.com>
2020-04-07 11:53:06 +0100

    update

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './card.styles.css';
 
-
 export const Card  = (props) => (
     <div className='card-container'>
         <img className='flag-container' alt={`${props.country.countryInfo.iso3}`} src={props.country.countryInfo.flag}/>
@@ -9,9 +8,9 @@ export const Card  = (props) => (
         <h2 >Total Cases: {props.country.cases}</h2>
         <h2 >Total Deaths: {props.country.deaths}</h2>
         <h3 >Total Active: {props.country.active}</h3>
-        <h3> Last Update: {props.country.updated}</h3>
+        <h3> Last Update: {Date()}</h3>
+        <h4>{console.log("first: ",props.country.updated)}</h4>
+        <h4>{console.log("second: ",`${props.country.updated}`)}</h4>
+        <h4>{console.log("third: ",(new Date().getTime(`${props.country.updated}`).toString()))}</h4>
     </div>
 )
-
-
-// <h3> Last Update: {props.country.updated.toLocaleString()}</h3>
