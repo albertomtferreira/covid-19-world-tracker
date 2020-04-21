5b3874171305e1714068c49c5edb551772789423
albertomtferreira <60177437+albertomtferreira@users.noreply.github.com>
2020-04-21 16:44:27 +0100

    card component update styles

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,18 +5,34 @@ export const Card  = (props) => {
 return(
     <div className='card-container'>
         <img className='flag-container' alt={`${props.country.countryInfo.iso3}`} src={props.country.countryInfo.flag}/>
+        
         <h1>Country: {props.country.country}</h1>
+        <div className='cases-container'>
         <h2>Total Cases: {props.country.cases}</h2>
-        <h2>Today Cases: {props.country.todayCases}</h2>
+        <span>Today Cases: {props.country.todayCases}</span>
+        </div>
+
+        <div className='cases-container'>
         <h2>Total Deaths: {props.country.deaths}</h2>
-        <h2>Today Deaths: {props.country.todayDeaths}</h2>
+        <span>Today Deaths: {props.country.todayDeaths}</span>
+        </div>
+
+        <div className='cases-container'>
         <h3>Total Active: {props.country.active}</h3> 
         <h3>Total Recovered: {props.country.recovered}</h3>
         <h3>Total Critical: {props.country.critical}</h3>
+        </div>
+
+        <div className='cases-container'>
         <h4>Cases per Million: {props.country.casesPerOneMillion}</h4>
         <h4>Deaths per Million: {props.country.deathsPerOneMillion}</h4>
+        </div>
+
+        <div className='cases-container'>
         <h5>Total Tests: {props.country.tests}</h5>
         <h5>Tests per Million: {props.country.testsPerOneMillion}</h5>
+        </div>
+        
     </div>
 )
 }
