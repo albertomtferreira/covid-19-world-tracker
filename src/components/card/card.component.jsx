6b00367f2d084b48bb51c1e545ee39fa6eb33f24
albertomtferreira <60177437+albertomtferreira@users.noreply.github.com>
2020-04-30 15:29:22 +0100

    update

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,42 +2,29 @@ import React from 'react';
 import './card.styles.css';
 
 export const Card  = (props) => {
-    return(
-    <div className='card-container'>
-        <img className='flag-container' alt={`${props.country.countryInfo.iso3}`} src={props.country.countryInfo.flag}/>
-        <h1>Country: {props.country.country}</h1>
-        <div className='cases-container'>
-        <h2>Total Cases: {props.country.cases}</h2>
-        <span>Today Cases: {props.country.todayCases}</span>
+    const lastUpdated = new Date(new Date(props.country.updated).toString());
+    return (
+        <div className='card-container'>
+            <h1>{console.log(lastUpdated)}</h1>
+            <img className='flag-container' alt={`${props.country.countryInfo.iso3}`} src={props.country.countryInfo.flag}/>
         </div>
-        <div className='cases-container'>
-        <h2>Total Deaths: {props.country.deaths}</h2>
-        <span>Today Deaths: {props.country.todayDeaths}</span>
-        </div>
-        <div className='cases-container'>
-        <h3>Total Active: {props.country.active}</h3> 
-        <h3>Total Recovered: {props.country.recovered}</h3>
-        <h3>Total Critical: {props.country.critical}</h3>
-        </div>
-        <div className='cases-container'>
-        <h4>Cases per Million: {props.country.casesPerOneMillion}</h4>
-        <h4>Deaths per Million: {props.country.deathsPerOneMillion}</h4>
-        </div>
-        <div className='cases-container'>
-        <h5>Total Tests: {props.country.tests}</h5>
-        <h5>Tests per Million: {props.country.testsPerOneMillion}</h5>
-        </div>
-    </div>
-)}
-
-// <h4>{new Date(new Date(props.country.updated).toString())}</h4>
-
-// <h4>{console.log(new Date(new Date(props.country.updated).toString()))}</h4>
-// <h3 >Total Active: {props.country.updated}</h3>
+    )
+}
 
 
-// render(){
-//     return(
+
+// <h1>{console.log(lastUpdated)}</h1>
+
+// <h1>{(lastUpdated)}</h1>
+
+// <h4>{new Date(new Date(props.country.updated).toString())}</h4> NOT WORKING AT ALL
+
+// <h4>{console.log(new Date(new Date(props.country.updated).toString()))}</h4> WORK FINE ON CONSOLE LOG
+// <h3 >Total Active: {props.country.updated}</h3> WORKS ON MS FORMAT
+
+
+// export onst Card  = (props) => {
+// return(
 //     <div className='card-container'>
 //         <img className='flag-container' alt={`${props.country.countryInfo.iso3}`} src={props.country.countryInfo.flag}/>
 //         <h1>Country: {props.country.country}</h1>
@@ -61,6 +48,9 @@ export const Card  = (props) => {
 //         <div className='cases-container'>
 //         <h5>Total Tests: {props.country.tests}</h5>
 //         <h5>Tests per Million: {props.country.testsPerOneMillion}</h5>
+//         </div>
+//         <div className='cases-container'>
+//         <h5>Total Active: {props.country.updated}</h5>
 //         </div>
 //     </div>
 // )}
